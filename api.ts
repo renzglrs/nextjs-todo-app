2a86f167aa7051dcb9267130ec376a6e006e1001
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-05-22 10:02:30 +0800

    update baseUrl

--- api.ts
+++ api.ts
@@ -1,7 +1,7 @@
 import { ITask } from "./types/task";
 
-// const baseUrl = 'http://localhost:3001';
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const baseUrl = 'http://localhost:3001';
+// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const getAllTodos = async (): Promise<ITask[]> => {
      const res = await fetch(`${baseUrl}/tasks`, { cache: 'no-store' });
